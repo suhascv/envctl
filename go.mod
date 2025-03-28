312666fae83620b7faf042af157b80a0109e8588
Suhas CV <suhas749@protonmail.com>
2025-03-27 23:17:57 -0700

    envctl init

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/suhascv/bet-practice
+module github.com/suhascv/envctl
 
 go 1.23.4
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("bet practice init")
+	fmt.Println("envctl init")
 }
